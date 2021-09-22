3dd81ff3082711b96655c14f867a0bb73157c648
Isaac Barrezueta <barrezuetai@allegheny.edu>
2021-09-21 20:33:45 -0400

    feature: block access to users page

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,6 +1,12 @@
-import {getAllUsers} from '../../repository/UserRepository'; // changed from ../../util/mongodb
+import { getAllUsers } from '../../repository/UserRepository'; // changed from ../../util/mongodb
+import { getSession } from 'next-auth/react';
+export default async (req, res) => {
+    const session = await getSession({req});
 
-export default async (_req, res) => {
-    const users = await getAllUsers();
-    res.json(users);
+    if (session && session.user.name == 'Isaac Barrezueta'){
+        const users = await getAllUsers();
+        res.json(users);
+    }
+    res.json({message: 'mind yo business'});
+    
 };
